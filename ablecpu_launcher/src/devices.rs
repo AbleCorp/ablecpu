695ba43c6f0a2097568a75334b8588a52ec1f90b
blackfur <64478051+theblackfurguy@users.noreply.github.com>
2021-06-25 21:36:35 +0200

    Untested code, it is late ok?

--- ablecpu_launcher/src/devices.rs
+++ ablecpu_launcher/src/devices.rs
@@ -10,6 +10,21 @@ pub(crate) struct TerminalIn {
 
 impl Device for TerminalOut {
     fn get_address_space(&self) -> (u64, u64) {
+        (131073, 131073)
+    }
+
+    fn load(&self, address: u64) -> u64 {
+        println!();
+        0
+    }
+
+    fn push(&self, address: u64, value: u64) {
+        print!("{}", std::str::from_utf8(&value.to_be_bytes()).unwrap())
+    }
+}
+
+impl Device for TerminalIn {
+    fn get_address_space(&self) -> (u64, u64) {
         todo!()
     }
 
