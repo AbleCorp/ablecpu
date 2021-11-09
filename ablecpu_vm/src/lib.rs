24ba54fbedf10be8e72ee15046a83cc3129dd900
TheBlackfurGuy <64478051+TheBlackfurGuy@users.noreply.github.com>
2021-11-09 19:46:44 +0100

    Commity witty

--- ablecpu_vm/src/lib.rs
+++ ablecpu_vm/src/lib.rs
@@ -6,12 +6,13 @@ pub fn get_version() -> &'static str {
     env!("CARGO_PKG_VERSION")
 }
 
-struct InstructionCache {
-    instructions: [(u8, u64, u64); 21845],
+pub struct InstructionCache {
+    pub instructions: [(u8, u64, u64); 21845],
 }
 
 impl InstructionCache {
-    fn new(raw: [u8; 371365]) -> InstructionCache {
+    fn new(raw: Box<[u8; 371365]>) -> InstructionCache {
+        println!("This still worked! 3");
         let mut i: usize = 0;
         let mut assembled: [(u8, u64, u64); 21845] = [(0, 0, 0); 21845];
         while i<371365 {
@@ -19,6 +20,7 @@ impl InstructionCache {
             let arg_one = u64::from_be_bytes(raw[i+1..=i+8].try_into().unwrap());
             let arg_two = u64::from_be_bytes(raw[i+9..=i+16].try_into().unwrap());
             assembled[i / 17] = (inst, arg_one, arg_two);
+            i+=17;
         }
 
         InstructionCache {
@@ -30,18 +32,30 @@ impl InstructionCache {
 pub struct Cpu {
     reg_zero: u64,
     data_cache: [u64; 65535],
-    instruction_cache: InstructionCache,
+    pub instruction_cache: InstructionCache,
     devices: Vec<Box<dyn Device>>
 }
 
 impl Cpu {
-    pub fn new(instructions: [u8; 371365]) -> Cpu{
+    pub fn new(instructions: Box<[u8; 371365]>) -> Cpu{
+        println!("This still worked! 2");
         Cpu{
             reg_zero: 65536,
             data_cache: [0; 65535],
             instruction_cache: InstructionCache::new(instructions),
             devices: Vec::new(),
         }
+    }
+}
+
+mod tests {
+    #[test]
+    fn it_works() {
+        println!("This still worked! 1");
+        let test_cpu = super::Cpu::new(Box::new([0; 371365]));
+        
+
+        println!("{:?}", test_cpu.instruction_cache.instructions[0]);
     }
 }
 
